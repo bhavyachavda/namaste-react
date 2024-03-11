77468879167a14f33ca755fab3fb237c0878296a
Bhavya Chavda <bhavyachavda55@gmail.com>
2024-03-11 08:01:44 +0530

    episode-02

--- App.js
+++ App.js
@@ -2,18 +2,28 @@
  *  <div id="parent">
  *      <div id="child">
  *          <h1>I am H1 tag</h1>
+ *          <h1>I am H2 tag</h1>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>I am H1 tag</h1>
+ *          <h1>I am H2 tag</h1>
  *      </div>
  *  </div>
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "hello from bavya"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
     React.createElement("h2", {}, "I am H2 tag"),
-  ])
-);
+  ]),
+]);
 const rootOfParent = ReactDOM.createRoot(document.getElementById("root"));
 rootOfParent.render(parent);
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namaste-React 🚀 
+# Namaste React 🚀 
+
+# parcel
+- Dev build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm -written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking -Remove unused Code
+- Different dev and prod bundles
